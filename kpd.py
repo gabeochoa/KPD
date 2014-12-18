bd2d9482ad1958638ceeab9e0ebab0f0f9497f27
Gabe <gabe1118@yahoo.com>
2014-12-18 00:48:54 -0500

    Fix time.sleep bug

--- kpd.py
+++ kpd.py
@@ -1,9 +1,9 @@
-import praw
-import urllib
-import os
-import time
-import requests
-from bs4 import BeautifulSoup
+import praw # reddit api
+import urllib # get images from url
+import os # makes directories
+import time # used to rerun mainFunc every 30 sec
+import requests # used to get html for BS
+from bs4 import BeautifulSoup # used to parse for imgur albums
 
 def mainfunc():
 	subreddits = ['kpics', 'apink', 'girlsday', 'AceOfAngels8']
@@ -69,7 +69,7 @@ def main():
 	while True:
 		mainfunc()
 		print('Time to sleep')
-		time.sleep(30000)
+		time.sleep(30)
 		print("Waking up")
 
 
